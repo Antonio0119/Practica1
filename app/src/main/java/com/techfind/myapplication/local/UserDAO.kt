61c810274cecf6a9f213d64cdcf7e1874f74cead
Antonio Aristizabal <aristizabal0119@gmail.com>
2022-02-21 11:06:51 -0500

    Se añade el repositorio de la base de datos

--- app/src/main/java/com/techfind/myapplication/local/UserDAO.kt
+++ app/src/main/java/com/techfind/myapplication/local/UserDAO.kt
@@ -1,7 +1,11 @@
 package com.techfind.myapplication.local
 
 import androidx.room.Dao
+import androidx.room.Insert
 
 @Dao
 interface UserDAO {
+
+    @Insert
+    fun newUser(user: User)
 }
---
+++ app/src/main/java/com/techfind/myapplication/repository/TechfindRepository.kt
@@ -0,0 +1,31 @@
+package com.techfind.myapplication.repository
+
+import com.techfind.myapplication.Techfind
+import com.techfind.myapplication.local.User
+import com.techfind.myapplication.local.UserDAO
+import java.sql.Types.NULL
+
+class TechfindRepository {
+
+    suspend fun newUser(
+        name: String,
+        email: String,
+        password: Int,
+        document: Int,
+        cel_number: Int
+
+    ) {
+        val user = User(
+            user_id = NULL,
+            name = name,
+            email = email,
+            password = password,
+            document = document,
+            cel_number = cel_number,
+
+        )
+
+        val userDAO: UserDAO = Techfind.database.UserDAO()
+        userDAO.newUser(user)
+    }
+}
