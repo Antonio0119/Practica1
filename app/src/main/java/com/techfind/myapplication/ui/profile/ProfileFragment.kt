72a6f0bd86e9df7c429209d3a056f2214152883c
Antonio Aristizabal <aristizabal0119@gmail.com>
2022-03-06 19:57:32 -0500

    Se agrega la funcionalidad del botón cerrar sesión en el fragment profile

--- app/src/main/java/com/techfind/myapplication/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/techfind/myapplication/ui/profile/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.techfind.myapplication.ui.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,8 +9,12 @@ import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.techfind.myapplication.databinding.ProfileFragmentBinding
 import com.techfind.myapplication.local.User
+import com.techfind.myapplication.ui.addservices.AddServiceFragmentDirections
+import com.techfind.myapplication.ui.login.LoginActivity
+import com.techfind.myapplication.ui.services.ServicesFragmentDirections
 
 class ProfileFragment : Fragment() {
 
@@ -29,7 +34,24 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        profileBinding.logoutButton.setOnClickListener {
+            goToLoginActivity()
 
+        }
+
+        /*with(profileBinding) {
+            logoutButton.setOnClickListener {
+                goToLoginActivity()
+            }
+        }*/
+
+
+    }
+
+    private fun goToLoginActivity() {
+        val intent = Intent(requireContext(), LoginActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
     }
 
 
