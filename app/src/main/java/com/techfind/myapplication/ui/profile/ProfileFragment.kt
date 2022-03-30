76f66773e014d392193da206a5ac316c83f866da
Julian <juliantaco23@gmail.com>
2022-03-29 23:48:07 -0500

    Cambia los datos tanto en la aplicacion como en la base de datos segun los ajustes hechos en la seccion de editar perfil. NO INCLUYE EMAILL Y PASSWORD ES UN PROCESO DIFERENTE Y MAS COMPLEJO

--- app/src/main/java/com/techfind/myapplication/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/techfind/myapplication/ui/profile/ProfileFragment.kt
@@ -1,30 +1,28 @@
 package com.techfind.myapplication.ui.profile
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.findNavController
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.techfind.myapplication.databinding.ProfileFragmentBinding
-import com.techfind.myapplication.local.User
-import com.techfind.myapplication.ui.addservices.AddServiceFragmentDirections
 import com.techfind.myapplication.ui.login.LoginActivity
-import com.techfind.myapplication.ui.services.ServicesFragmentDirections
 
 class ProfileFragment : Fragment() {
 
     private lateinit var profileBinding: ProfileFragmentBinding
     private lateinit var profileViewModel: ProfileViewModel
-    private lateinit var user: User
     private lateinit var auth: FirebaseAuth
+    val db = Firebase.firestore
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,20 +36,90 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        auth = Firebase.auth
-        profileBinding.logoutButton.setOnClickListener {
-            auth.signOut()
-            goToLoginActivity()
-
+        profileViewModel.msgDone.observe(viewLifecycleOwner) { result ->
+            Toast.makeText(requireContext(),result.toString(),Toast.LENGTH_SHORT).show()
         }
 
-        /*with(profileBinding) {
+        profileViewModel.statusDone.observe(viewLifecycleOwner){ result ->
+            changeInformationInServer(result)
+        }
+        auth = Firebase.auth
+        with(profileBinding) {
             logoutButton.setOnClickListener {
+                auth.signOut()
                 goToLoginActivity()
             }
-        }*/
+            saveButton.setOnClickListener {
+                val name = editnameEditText.text.toString()
+                val email = editemailEditText.text.toString()
+                val password = editpasswordEditText.text.toString()
+                val document = editdocumentEditText.text.toString()
+                val number = editnumberEditText.text.toString()
+                profileViewModel.validation(name,email,password,document,number)
+            }
+        }
+    }
 
+    private fun changeInformationInServer(result: ArrayList<String>?) {
+        if (result != null) {
+            for (element in result) {
+                if (element == "name"){
+                    db.collection("users")
+                        .document(auth.currentUser?.uid.toString())
+                        .update("name"
+                            ,profileBinding.editnameEditText.text.toString())
+                }
+                if (element == "email"){
+                    val email = profileBinding.editemailEditText.text.toString()
+                    // need to sign user in immediately before updating the email
+                    auth.signInWithEmailAndPassword("currentEmail","currentPassword")
+                        .addOnCompleteListener() { task ->
+                            if (task.isSuccessful) {
+                                // Sign in success now update email
+                                auth.currentUser!!.updateEmail(email)
+                                    .addOnCompleteListener{ task ->
+                                        if (task.isSuccessful) {
+                                            Toast.makeText(requireContext(),"Se ha podido cambiar el correo exitosamente ",Toast.LENGTH_SHORT).show()
+                                        }else{
+                                            Toast.makeText(requireContext(),"No se ha podido cambiar el correo ",Toast.LENGTH_SHORT).show()
+                                        }
+                                    }
+                            } else {
+                                // sign in failed
+                            }
+                        }
 
+                    db.collection("users")
+                        .document(auth.currentUser?.uid.toString())
+                        .update("email"
+                            ,profileBinding.editemailEditText.text.toString())
+
+                }
+                if (element == "password"){
+                    auth.currentUser?.updatePassword(
+                        profileBinding.editpasswordEditText.text.toString())
+                    db.collection("users")
+                        .document(auth.currentUser?.uid.toString())
+                        .update("password"
+                            ,profileBinding.editpasswordEditText.text.toString())
+                }
+                if (element == "document"){
+                    db.collection("users")
+                        .document(auth.currentUser?.uid.toString())
+                        .update("document"
+                            ,profileBinding.editdocumentEditText.text.toString().toInt())
+                }
+                if (element == "number"){
+                    db.collection("users")
+                        .document(auth.currentUser?.uid.toString())
+                        .update("cel_number"
+                            ,profileBinding.editnumberEditText.text.toString().toInt())
+                }
+            }
+        }
+        else{
+            Log.d("1","Vacio")
+        }
     }
 
     private fun goToLoginActivity() {
--- app/src/main/java/com/techfind/myapplication/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/techfind/myapplication/ui/profile/ProfileViewModel.kt
@@ -1,9 +1,68 @@
 package com.techfind.myapplication.ui.profile
 
+import android.util.Log
+import android.util.Patterns
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.techfind.myapplication.local.repository.TechfindRepository
 
 class ProfileViewModel : ViewModel() {
 
-    val techfindRepository = TechfindRepository()
+    private val msg: MutableLiveData<String> = MutableLiveData()
+    val msgDone: LiveData<String> = msg
+    private val status: MutableLiveData<ArrayList<String>> = MutableLiveData()
+    val statusDone: LiveData<ArrayList<String>> = status
+
+    fun validation(name: String, email: String, password: String, document: String, number: String) {
+        status.value?.clear()
+        val aux :MutableList<String> = mutableListOf()
+        aux.clear()
+
+        if (name.isNotEmpty() || email.isNotEmpty() || password.isNotEmpty()
+            || document.isNotEmpty() || number.isNotEmpty()
+        ) {
+            Log.d("1","Algun dato")
+            if(name.isNotEmpty()){
+                aux.add("name")
+            }
+            if (email.isNotEmpty()){
+                if(emailValidation(email)) {
+                    aux.add("email")
+                }
+                else {
+                    aux.clear()
+                    msg.value = "Debe digitar un correo electronico valido"
+                    return
+
+                }
+            }
+            if(password.isNotEmpty()) {
+                if (password.length >= 6) {
+                    aux.add("password")
+                } else {
+                    aux.clear()
+                    msg.value = "La contraseña debe tener mínimo 6 dígitos"
+                    return
+                }
+            }
+            if (document.isNotEmpty()){
+                aux.add("document")
+            }
+            if (number.isNotEmpty()){
+                aux.add("number")
+            }
+            status.postValue(aux as ArrayList<String>?)
+            msg.value = "Se ha cambiado la informacion correctamente"
+        } else {
+            // si no digita alguno de los campos
+            status.value?.clear()
+            msg.value = "Debe digitar alguno de los campos"
+            return
+        }
+
+    }
+
+    private fun emailValidation(email: String): Boolean {
+        return email.isNotEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
 }
