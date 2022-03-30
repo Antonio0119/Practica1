0c2621d03a9f58acb41161623cf4649d7e07fab3
Julian <juliantaco23@gmail.com>
2022-03-29 20:52:50 -0500

    Solucion error Login

--- app/src/main/java/com/techfind/myapplication/ui/login/LoginActivity.kt
+++ app/src/main/java/com/techfind/myapplication/ui/login/LoginActivity.kt
@@ -9,7 +9,6 @@ import androidx.lifecycle.ViewModelProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-import com.techfind.myapplication.ui.main.MainActivity
 import com.techfind.myapplication.ui.register.RegisterActivity
 import com.techfind.myapplication.databinding.ActivityLoginBinding
 import com.techfind.myapplication.ui.bottom.BottomActivity
@@ -18,7 +17,6 @@ class LoginActivity : AppCompatActivity() {
 
     private lateinit var loginBinding: ActivityLoginBinding
     private lateinit var loginViewModel: LoginViewModel
-    private var statusO : Int = 0
     private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,40 +31,26 @@ class LoginActivity : AppCompatActivity() {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
-
-        loginViewModel.statusDone.observe(this@LoginActivity){ result ->
-            statusO = result
-        }
-
-        loginViewModel.msgDone.observe(this) { result ->
-            Toast.makeText(this@LoginActivity,result,Toast.LENGTH_SHORT).show()
-        }
-
         with(loginBinding){
             loginButton.setOnClickListener{
                 val email = emailEditText.text.toString()
                 val password = passwordEditText.text.toString()
-
-                loginViewModel.Validate(email,password)
-                if (statusO == 1){
-                    auth.signInWithEmailAndPassword(email, password)
-                        .addOnCompleteListener() { task ->
-                            if (task.isSuccessful) {
-                                // Sign in success, update UI with the signed-in user's information
-                                val intent = Intent(this@LoginActivity, BottomActivity::class.java)
-                                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                                startActivity(intent)
-                            } else {
-                                // If sign in fails, display a message to the user.
-                                Log.w("TAG", "signInWithEmail:failure", task.exception)
-                                Toast.makeText(baseContext, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show()
+                Log.d("2","what da hell its going on")
+                auth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener() { task ->
+                        Log.d("2","what its going on")
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            val intent = Intent(this@LoginActivity, BottomActivity::class.java)
+                            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                            startActivity(intent)
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "signInWithEmail:failure", task.exception)
+                            Toast.makeText(baseContext, "Usuario o contraseña incorrectos",
+                            Toast.LENGTH_SHORT).show()
                             }
                         }
-                    //val intent =
-                      //  Intent(this@LoginActivity, BottomActivity::class.java)
-                    //startActivity(intent)
-                }
             }
         }
 
--- app/src/main/java/com/techfind/myapplication/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/techfind/myapplication/ui/login/LoginViewModel.kt
@@ -9,32 +9,4 @@ import com.techfind.myapplication.local.repository.TechfindRepository
 
 class LoginViewModel: ViewModel() {
 
-    private val emailActive: MutableLiveData<String> = MutableLiveData()
-    val emailActiveDone: LiveData<String> = emailActive
-    private val msg: MutableLiveData<String> = MutableLiveData()
-    val msgDone: LiveData<String> = msg
-
-    private val status: MutableLiveData<Int> = MutableLiveData()
-    val statusDone: LiveData<Int> = status
-
-    private val userRepository = TechfindRepository()
-
-    fun Validate(email:String,password:String){
-        val user: User = userRepository.searchUser(email)
-        if(user != null) {
-            if (user.password == password) {
-                msg.value = "Inicio sesion"
-                status.value = 1
-                emailActive.value = email
-            } else {
-                msg.value = "Usuario o Contraseña incorrecta"
-                status.value = 0
-            }
-        }else {
-            Log.d("aa","ssds")
-            msg.value = "Usuario o Contraseña incorrecta"
-            status.value = 0
-        }
-
-    }
 }
